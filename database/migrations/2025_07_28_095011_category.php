69745c91d945b8f8729462ee262f21bf6af36c0a
ErvinTyx <ervinteohyx@gmail.com>
2025-07-28 22:50:17 +0800

    Added table for categories

--- database/migrations/2025_07_28_095011_category.php
+++ database/migrations/2025_07_28_095011_category.php
@@ -12,6 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         //
+        Schema::create("categories", function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+        });
     }
 
     /**
@@ -20,5 +24,6 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::dropIfExists("categories");
     }
 };
